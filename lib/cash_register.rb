5ac51c30da63b6dfdc96927d3ff2ec579574ec74
linkcoding <kenny.lin.91@gmail.com>
2017-07-19 13:49:09 -0400

    add cash register class

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :items, :total
+  attr_reader :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity=1)
+    self.total += price * quantity.to_f
+    quantity.times do
+      self.items << title
+    end
+  end
+
+  def apply_discount
+    unless self.discount == 0
+      self.total = self.total * (1 - self.discount/100.0)
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total = 0
+  end
+
+end
